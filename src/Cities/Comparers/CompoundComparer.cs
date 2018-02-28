e70102b5f647cb09c08a3b7413421f03272461bc
James Cook <jamescook.work@gmail.com>
2018-02-27 19:53:10 -0500

    Basic studio complete. Bonus mission not complete.

---
+++ src/Cities/Comparers/CompoundComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cities.Comparers
+{
+    class CompoundComparer : IComparer<City>
+    {
+        public IList<IComparer<City>> Comparers { get; set; } = new List<IComparer<City>>();
+        private int result = 1;
+
+        public int Compare(City x, City y)
+        {
+            while (result != 0)
+            {
+                IComparer<City> comparer1 = Comparers[0];
+                
+                result = ((x, y).comparer1);
+                return result;
+
+            }
+            IComparer<City> comparer2 = Comparers[1];
+            return string.Compare(x.Name, y.Name);
+        }
+    }
+}
